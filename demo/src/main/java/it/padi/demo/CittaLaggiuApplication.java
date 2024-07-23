02d3739743bb87953c864efbe409a662a7e1fa73
Federico-Massaccesi <fede99ita26@gmail.com>
2024-07-23 11:36:38 +0200

    start

---
+++ demo/src/main/java/it/padi/demo/CittaLaggiuApplication.java
@@ -0,0 +1,13 @@
+package it.padi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CittaLaggiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CittaLaggiuApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/it/padi/demo/CittaLaggiuApplicationTests.java
@@ -0,0 +1,13 @@
+package it.padi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CittaLaggiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
