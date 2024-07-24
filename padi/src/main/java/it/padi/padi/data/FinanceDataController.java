f94a5d3f075693e0ce449952609100a34285560e
Federico-Massaccesi <fede99ita26@gmail.com>
2024-07-24 19:45:28 +0200

    aggiunta cors da testare in frontend

--- padi/src/main/java/it/padi/padi/data/FinanceDataController.java
+++ padi/src/main/java/it/padi/padi/data/FinanceDataController.java
@@ -2,7 +2,7 @@ package it.padi.padi.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/finance")
 public class FinanceDataController {
--- padi/src/main/java/it/padi/padi/user/GuestController.java
+++ padi/src/main/java/it/padi/padi/user/GuestController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/guests")
 public class GuestController {
